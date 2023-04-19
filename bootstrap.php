39dc9540175a661645bd35df1ae9917b182d33c6
TiboMertens <tibomertens25@gmail.com>
2023-04-19 16:25:13 +0200

    chore: added .gitignore

---
+++ bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+spl_autoload_register(function ($class) {
+    require_once(__DIR__ . DIRECTORY_SEPARATOR . "classes" . DIRECTORY_SEPARATOR . $class . ".php");
+});
+
+//autostart session
+session_start();
