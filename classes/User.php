ab58bbe7620ddbe24abb70c8378636ad89f7e5af
TiboMertens <tibomertens25@gmail.com>
2023-04-20 14:14:16 +0200

    feat: user can now register

--- classes/User.php
+++ classes/User.php
@@ -2,6 +2,9 @@
 class User {
     private int $id;
     private string $language;
+    private string $username;
+    private string $email;
+    private string $password;
 
 
     /**
@@ -22,5 +25,84 @@ class User {
         $this->language = $language;
 
         return $this;
+    }
+
+    /**
+     * Get the value of username
+     */ 
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @return  self
+     */ 
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        //validate email
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+
+            return $this;
+        } else {
+            throw new Exception("Sorry, dit is geen geldig emailadres.");
+        }
+
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        //hash password with a factor of 12
+        $password = password_hash($password, PASSWORD_DEFAULT, ['cost' => 12]);
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function save(){
+        $conn = Db::getInstance();
+        $statement = $conn->prepare("INSERT INTO users (language, username, email, password) values (:language, :username, :email, :password)");
+        $statement->bindValue(":language", $this->getLanguage());
+        $statement->bindValue(":username", $this->getUsername());
+        $statement->bindValue(":email", $this->getEmail());
+        $statement->bindValue(":password", $this->getPassword());
+        $result = $statement->execute();
+        return $result;
     }
 }
--- register.php
+++ register.php
@@ -1,5 +1,22 @@
 <?php include_once(__DIR__ . DIRECTORY_SEPARATOR . "bootstrap.php");
+//get language from session
+$language = $_SESSION['language'];
 
+if (!empty($_POST)) {
+    try {
+        $user = new User();
+        $user->setLanguage($language);
+        $user->setUsername($_POST['username']);
+        $user->setEmail($_POST['email']);
+        $user->setPassword($_POST['password']);
+        $user->save();
+    
+        header("Location: home.php");
+    } catch (Throwable $e) {
+        $error = $e->getMessage();
+    }
+
+}
 
 ?>
 <!DOCTYPE html>
@@ -9,11 +26,79 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&family=Yeseva+One&display=swap" rel="stylesheet">
 </head>
+<style>
+    html {
+        height: 100%;
+        /* Fallback CSS for IE 4-6 and older browsers. Note: Without this setting, body below cannot achieve 100% height. */
+        height: 100vh;
+        /* Overrides 100% height in modern HTML5 browsers and uses the viewport's height. Only works in modern HTML5 browsers */
+    }
 
-<body>
+    body {
+        height: auto;
+        /* Allows content to grow beyond the page without overflow */
+        width: auto;
+        /* Allows content to grow beyond the page without overflow */
+        min-height: 100%;
+        /* Starts web page with 100% height. Fallback for IE 4-6 and older browsers */
+        min-height: 100vh;
+        /* Starts web page with 100% height. Uses the viewport's height. Only works in modern HTML5 browsers */
+        overflow-y: scroll;
+        /* Optional: Adds an empty scrollbar to the right margin in case content grows vertically, creating a scrollbar.  Allows for better width calculations, as the browser pre-calculates width before scrollbar appears, avoiding page content shifting.*/
+        margin: 0;
+        padding: 0;
+    }
 
+    input[type="radio"] {
+        appearance: none;
+        -webkit-appearance: none;
+        border-radius: 50%;
+        border: 0px solid #FFF;
+        background: #F5F3F3;
+        /* The outline will be the outer circle */
+        outline: 1px solid #81CCDE;
+    }
+
+    input[type="radio"]:checked {
+        border: 3px solid #FFF;
+        background: #81CCDE;
+    }
+</style>
+
+<body class="bg-[#F5F3F3] ml-auto mr-auto max-w-[500px]" style="height: 100%;">
+    <section class="flex justify-center items-center" style="height:35%">
+        <p>LOGO</p>
+    </section>
+    <main class="bg-[#A5CF93] rounded-t-[30px] pl-[24px] pr-[24px]" style="height: 65%">
+        <form action="" method="post">
+            <h1 class="text-[26px] text-white pt-[44px]">Registreer u</h1>
+            <div class="text-white text-[20px]">
+                <div class="mt-[16px]">
+                    <input type="text" placeholder="Gebruikersnaam" name="username" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px]">
+                </div>
+                <div class="mt-[32px]">
+                    <input type="text" placeholder="Email" name="email" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px]">
+                </div>
+                <div class="mt-[32px]">
+                    <input type="password" placeholder="Wachtwoord" name="password" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px]">
+                </div>
+            </div>
+            <!-- If there is an error, show it -->
+            <?php if (isset($error)) : ?>
+                <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+            <?php endif; ?>
+            <div class="mt-[32px]">
+                <input type="submit" value="GA VERDER" class="h-[48px] bg-[#81CCDE] w-full rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px]">
+            </div>
+        </form>
+    </main>
 </body>
 
 </html>
