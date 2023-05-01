1061c3e4d732ffe2cdddef3b386676fe12517e57
TiboMertens <tibomertens25@gmail.com>
2023-05-02 00:04:18 +0200

    chore: deleted unnecessary code

--- avgReadings.php
+++ avgReadings.php
@@ -1,6 +1,4 @@
 <?php
-// https://betterstack.com/community/guides/scaling-php/php-scheduled-tasks/
-
 include_once(__DIR__ . DIRECTORY_SEPARATOR . "bootstrap.php");
 
 //do 7 times
@@ -18,42 +16,3 @@ $fiveDaysAgoData = $avg[date('Y-m-d', strtotime("-5 days"))];
 $sixDaysAgoData = $avg[date('Y-m-d', strtotime("-6 days"))];
 
 var_dump($todayData, $yesterdayData, $twoDaysAgoData, $threedaysAgoData, $fourDaysAgoData, $fiveDaysAgoData, $sixDaysAgoData);
-
-
-// // Get the current time and the target time (11:59 PM)
-// $target_time = strtotime('today 23:59:00');
-// $current_time = time();
-
-// // Calculate the delay in seconds
-// $delay = $target_time - $current_time;
-
-// // Output the JavaScript code to reload the page after the delay
-// echo '<script>setTimeout(function(){location.reload();}, ' . $delay * 1000 . ');</script>';
-
-// Check if the current time is after the target time
-// if ($current_time == $target_time) {
-//     //get all moestuinen
-//     $moestuinen = Moestuin::getAllMoestuinen();
-
-//     foreach ($moestuinen as $moestuin) {
-//         //for each moestuin, get all sensors
-//         $sensors = Moestuin::getAvgSensors($moestuin['id']);
-//         foreach ($sensors as $sensor) {
-//             //get all readings of this day
-//             $moestuin_id = $moestuin['id'];
-//             $sensor_id = $sensor['sensor_id'];
-
-//             $readings = Moestuin::getReadings($moestuin_id, $sensor_id);
-
-//             //get the average of all readings
-//             $avg = 0;
-//             foreach ($readings as $reading) {
-//                 $avg += $reading['data'];
-//             }
-//             $avg = $avg / count($readings);
-
-//             //insert the average into the database
-//             Moestuin::insertAvg($avg, $moestuin_id, $sensor_id);
-//         }
-//     }
-// }
--- classes/Moestuin.php
+++ classes/Moestuin.php
@@ -274,27 +274,6 @@ class Moestuin
         return $result;
     }
 
-    // public static function getAllMoestuinen()
-    // {
-    //     $conn = Db::getInstance();
-    //     $statement = $conn->prepare("SELECT id FROM moestuin");
-    //     $statement->execute();
-    //     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-    //     return $result;
-    // }
-
-    // public static function getReadings($moestuin_id, $sensor_id)
-    // {
-    //     $conn = Db::getInstance();
-    //     $statement = $conn->prepare("SELECT * FROM readings WHERE moestuin_id = :moestuin_id AND sensor_id = :sensor_id AND DATE(date_time) = :currentDay");
-    //     $statement->bindValue(":moestuin_id", $moestuin_id);
-    //     $statement->bindValue(":sensor_id", $sensor_id);
-    //     $statement->bindValue(":currentDay", date('Y-m-d'));
-    //     $statement->execute();
-    //     $result = $statement->fetchAll(PDO::FETCH_ASSOC);
-    //     return $result;
-    // }
-
     public static function getAvg($date)
     {
         //get the average of the data from the current day
@@ -305,27 +284,4 @@ class Moestuin
         $result = $statement->fetch(PDO::FETCH_ASSOC);
         return $result;
     }
-
-    // public static function getAvgSensors($moestuin_id)
-    // {
-    //     //get all sensors from the moestuin
-    //     $conn = Db::getInstance();
-    //     $statement = $conn->prepare("SELECT sensor_id FROM moestuin_sensor WHERE moestuin_id = :moestuin_id");
-    //     $statement->bindValue(":moestuin_id", $moestuin_id);
-    //     $statement->execute();
-    //     $sensors = $statement->fetchAll(PDO::FETCH_ASSOC);
-    //     return $sensors;
-    // }
-
-    // public static function insertAvg($avg, $moestuin_id, $sensor_id)
-    // {
-    //     //insert the average of the sensor in the database
-    //     $conn = Db::getInstance();
-    //     $statement = $conn->prepare("INSERT INTO data (date, avg_data, moestuin_id, sensor_id) VALUES (:date, :avg, :moestuin_id, :sensor_id)");
-    //     $statement->bindValue(":date", date('Y-m-d'));
-    //     $statement->bindValue(":avg", $avg);
-    //     $statement->bindValue(":moestuin_id", $moestuin_id);
-    //     $statement->bindValue(":sensor_id", $sensor_id);
-    //     $statement->execute();
-    // }
 }
