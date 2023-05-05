9731e48890aee4e573d2806890e984422b02ef43
TiboMertens <tibomertens25@gmail.com>
2023-05-05 15:49:17 +0200

    fix: price & back button

--- shop.php
+++ shop.php
@@ -28,7 +28,10 @@ if (isset($_SESSION['loggedin'])) {
 </head>
 
 <body class="bg-[#F9F9F9]">
-    <h1 class="text-center my-[42px] text-[26px] font-semibold">Winkel</h1>
+    <div class="flex items-center justify-center">
+        <a href="home.php?id=<?php echo $moestuin_id ?>"><i class="fa-solid fa-arrow-left fa-xl mr-2"></i></a>
+        <h1 class="text-center my-[42px] text-[26px] lg:text-[28px] font-semibold">Winkel</h1>
+    </div>
     <section>
         <h2 class=" text-[20px] text-black mb-2 ml-5">Individuele sensoren</h2>
         <div class="flex overflow-x-auto bg-white m-5 mt-0 pt-7 px-7 pb-4 rounded-lg">
@@ -37,6 +40,7 @@ if (isset($_SESSION['loggedin'])) {
                     <a href="#">
                         <img src="<?php echo htmlspecialchars($sensor['cover_url']); ?>" alt="prompt" class="w-[120px] h-[120px] object-cover object-center rounded-lg">
                         <h2 class="text-black text-[14px] mt-2"><?php echo htmlspecialchars($sensor['name']) ?></h2>
+                        <p class="font-light text-[12px]"><?php echo "€" .  $sensor['price'] ?></p>
                     </a>
                 <?php endforeach; ?>
             </div>
@@ -50,13 +54,14 @@ if (isset($_SESSION['loggedin'])) {
                     <a href="sensorDetails.php?id=<?php echo htmlspecialchars($count++) ?>&packet=true">
                         <img src="<?php echo htmlspecialchars($packet['cover_url']); ?>" alt="prompt" class="w-[120px] h-[120px] object-cover object-center rounded-lg">
                         <h2 class="text-black text-[14px] mt-2"><?php echo htmlspecialchars($packet['name']) ?></h2>
+                        <p class="font-light text-[12px]"><?php echo "€" .  $packet['price'] ?></p>
                     </a>
                 <?php endforeach; ?>
             </div>
         </div>
     </section>
     <form action="" method="post" class="flex flex-col items-center">
-            <input type="submit" value="OVERZICHT BETALING" name="volgende" id="name" class="h-[48px] bg-[#81CCDE] w-[460px] rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px] mb-[32px]">
+        <input type="submit" value="OVERZICHT BETALING" name="volgende" id="name" class="h-[48px] bg-[#81CCDE] w-[460px] rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px] mb-[32px]">
     </form>
 </body>
 
