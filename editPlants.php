3aa1e5017163c38bc5060fd29bf93c26c5b584ab
TiboMertens <tibomertens25@gmail.com>
2023-05-07 19:08:25 +0200

    style: added frontend

--- editPlants.php
+++ editPlants.php
@@ -54,7 +54,7 @@ if (isset($_SESSION['loggedin'])) {
 
     $newPlants = $_SESSION['selectedPlants'];
 
-    if (!empty($_POST['save'])) {
+    if (isset($_POST['save'])) {
         $moestuin = new Moestuin();
         $moestuin->setPlants($newPlants);
         $moestuin->addPlants($moestuin_id);
@@ -68,6 +68,12 @@ if (isset($_SESSION['loggedin'])) {
         $moestuin = new Moestuin();
         $moestuin->delete($moestuin_id);
         header('Location: home.php');
+    }
+
+    if ($details['is_serre'] == 1) {
+        $border = '#81CCDE';
+    } else {
+        $border = '#496149';
     }
 } else {
     header('Location: login.php');
@@ -83,6 +89,7 @@ if (isset($_SESSION['loggedin'])) {
     <title>pas je moestuin aan</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://kit.fontawesome.com/c2626c7e45.js" crossorigin="anonymous"></script>
+    <script src="js/overlay.js" defer></script>
     <link rel="stylesheet" href="css/create.css">
     <link rel="stylesheet" href="css/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -94,83 +101,80 @@ if (isset($_SESSION['loggedin'])) {
     <div id="container" style="height: 100%">
         <div id="container2" class="">
             <div>
-                <h1 class="font-bold text-[26px] mb-2 text-center"> <?php echo htmlspecialchars($name) ?> </h1>
+                <h1 class="font-bold text-[26px] text-center mb-[12px]"> <?php echo htmlspecialchars($name) ?> </h1>
                 <div>
-                    <div class="w-[372px] md:w-[452px] lg:w-[522px] h-[520px] bg-[#808080] flex flex-col justify-between">
-                        <div class="flex-grow-1">
-                            <h2 class="font-bold text-[20px] text-white ml-[24px] mt-[8px]">Planten</h2>
-                            <div class="flex flex-wrap ml-[24px]">
-                                <?php foreach ($plants as $plant) : ?>
-                                    <div class="mr-[24px]">
-                                        <form action="" method="post">
-                                            <div>
-                                                <button class="text-right" name="deletePlant" value="<?php echo $plant['name'] ?>">
-                                                    <div class="relative top-[12px] left-[88px]"><i class="fa-solid fa-circle-minus text-red-600"></i></div>
-                                                </button>
-                                                <div class="h-[96px] w-[96px] bg-black flex justify-center items-center"></div>
-                                                <p class="pt-1 text-[16px] font-medium text-white max-w-[96px] break-all text-left"><?php echo wordwrap($plant['name'], 10, '-') ?></p>
-                                            </div>
-                                        </form>
-                                    </div>
-                                <?php endforeach; ?>
-                                <?php foreach ($newPlants as $plant) : ?>
-                                    <div class="mr-[24px]">
-                                        <form action="" method="post">
-                                            <div>
-                                                <button class="text-right" name="deleteNewPlant" value="<?php echo $plant ?>">
-                                                    <div class="relative top-[12px] left-[88px]"><i class="fa-solid fa-circle-minus text-red-600"></i></div>
-                                                </button>
-                                                <div class="h-[96px] w-[96px] bg-black flex justify-center items-center"></div>
-                                                <p class="pt-1 text-[16px] font-medium text-white max-w-[96px] break-all text-left"><?php echo wordwrap($plant, 10, '-') ?></p>
-                                            </div>
-                                        </form>
-                                    </div>
-                                <?php endforeach; ?>
-                                <div class="mt-[24px]">
-                                    <div>
-                                        <div class="h-[96px] w-[96px] bg-black flex justify-center items-center cursor-pointer" id="add"><i class="fa-solid fa-plus fa-xl" style="color: #ffffff;"></i></div>
-                                    </div>
+                    <div class="w-[372px] md:w-[452px] lg:w-[522px] h-[520px] bg-[#739B72] flex flex-col justify-between border-[8px] border-dashed" style="border-color: <?php echo $border ?>;"">
+                        <div class=" flex-grow-1 max-h-[400px] overflow-y-auto ml-[18px] md:ml-[18px] lg:ml-[8px]">
+                        <h2 class="font-regular text-[18px] text-white ml-[24px] mt-[24px]">Planten</h2>
+                        <div class="flex flex-wrap ml-[24px]">
+                            <?php foreach ($plants as $plant) : ?>
+                                <div class="mr-[12px]">
+                                    <form action="" method="post">
+                                        <div>
+                                            <button class="text-right" name="deletePlant" value="<?php echo $plant['name'] ?>">
+                                                <div class="relative bottom-[7px] left-[73px] z-10"><i class="fa-solid fa-circle-minus text-white"></i></div>
+                                            </button>
+                                            <div class="h-[82px] w-[82px] relative bottom-[20px] bg-[#5C7C5B] flex justify-center items-center border-2 rounded-lg border-[#496048]"><img class="w-[55px]" src="uploads/<?php echo $plant['cover_url'] ?>" alt="<?php echo $plant['name'] ?>"></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            <?php endforeach; ?>
+                            <?php foreach ($newPlants as $plant) : ?>
+                                <div class="mr-[12px]">
+                                    <form action="" method="post">
+                                        <div>
+                                            <button class="text-right" name="deleteNewPlant" value="<?php echo $plant ?>">
+                                                <div class="relative bottom-[7px] left-[73px] z-10"><i class="fa-solid fa-circle-minus text-white"></i></div>
+                                            </button>
+                                            <div class="h-[82px] w-[82px] relative bottom-[20px] bg-[#5C7C5B] flex justify-center items-center border-2 rounded-lg border-[#496048]"><img class="w-[55px]" src="uploads/<?php echo $plant ?>.svg" alt="<?php echo $plant ?>"></div>
+                                        </div>
+                                    </form>
+                                </div>
+                            <?php endforeach; ?>
+                            <div class="mt-[4px]">
+                                <div>
+                                    <div class="h-[82px] w-[82px] bg-white flex justify-center border-2 rounded-lg border-[#496048] items-center cursor-pointer" id="add"><i class="fa-solid fa-plus fa-xl" style="color: #000000;"></i></div>
                                 </div>
                             </div>
-
                         </div>
-                        <form action="" method="post" class="flex flex-col items-center">
-                            <input type="submit" value="GEREED" name="save" id="gereed" class="h-[48px] bg-[#81CCDE] w-[324px] md:w-[404px] lg:w-[472px] rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px] mb-[12px]">
-                            <input type="submit" value="verwijder moestuin" name="delete" id="verwijderen" class="mb-[14px] text-red-600 font-semibold hover:text-red-800 cursor-pointer">
-                        </form>
+
                     </div>
+                    <form action="" method="post" class="flex flex-col items-center">
+                        <button name="save" class="h-[48px] text-center bg-[#81CCDE] mt-[20px] w-[270px] md:w-[365px] lg:w-[458px] rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px] pt-[4px]" href="home.php?id=<?php echo $moestuin_id ?>">GEREED</button>
+                        <button name="delete" class="text-white font-semibold mt-[12px] mb-[20px] text-[12px]">Verwijder moestuin</button>
+                    </form>
                 </div>
             </div>
         </div>
-        <section class="flex justify-center items-center" style="height: 100%;">
-            <section id="add-section" class="hidden z-50 w-[522px]" style="height: 100%;">
-                <section class="flex justify-center items-center" style="height: 30%;" id="close"></section>
-                <section class="bg-[#A5CF93] rounded-t-[30px] pl-[24px] pr-[24px] overflow-y-auto" style="height: 70%">
-                    <i class="fa-solid fa-arrow-left fa-lg pt-[32px] cursor-pointer" style="color: #ffffff;" id="close2"></i>
-                    <form action="" method="get" id="search-form" data-id="<?php echo $state ?>">
-                        <input type="hidden" name="id" value="<?php echo $moestuin_id ?>">
-                        <input type="text" placeholder="Zoek plant" name="q" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px] mt-[12px]">
-                    </form>
-                    <div class="flex flex-wrap justify-center mt-[32px]">
-                        <?php if (empty($allPlants)) : ?>
-                            <p class="text-white font-bold text-[18px]">Geen planten gevonden.</p>
-                        <?php endif ?>
-                        <?php foreach ($allPlants as $plant) : ?>
-                            <div class="mr-[24px] mb-[24px]">
-                                <form action="" method="post">
-                                    <button type="submit" name="btn" value="<?php echo $plant['name'] ?>">
-                                        <div class="pt-[8px] cursor-pointer">
-                                            <div class="h-[96px] w-[96px] bg-black flex justify-center items-center"></div>
-                                            <p class="pt-1 text-[16px] font-medium text-white max-w-[96px] break-all text-left"><?php echo wordwrap($plant['name'], 10, '-') ?></p>
-                                        </div>
-                                    </button>
-                                </form>
-                            </div>
-                        <?php endforeach ?>
-                    </div>
-                </section>
+    </div>
+    <section class="flex justify-center items-center" style="height: 100%;">
+        <section id="add-section" class="hidden z-50 w-[372px] md:w-[452px] lg:w-[522px]" style="height: 100%;">
+            <section class="flex justify-center items-center" style="height: 30%;" id="close"></section>
+            <section class="bg-[#A5CF93] rounded-t-[30px] pl-[24px] pr-[24px] overflow-y-auto" style="height: 70%">
+                <i class="fa-solid fa-arrow-left fa-lg pt-[32px] cursor-pointer" style="color: #ffffff;" id="close2"></i>
+                <form action="" method="get" id="search-form" data-id="<?php echo $state ?>">
+                    <input type="hidden" name="id" value="<?php echo $moestuin_id ?>">
+                    <input type="text" placeholder="Zoek plant" name="q" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px] mt-[12px]">
+                </form>
+                <div class="flex flex-wrap justify-center mt-[32px]">
+                    <?php if (empty($allPlants)) : ?>
+                        <p class="text-white font-bold text-[18px]">Geen planten gevonden.</p>
+                    <?php endif ?>
+                    <?php foreach ($allPlants as $plant) : ?>
+                        <div class="mr-[12px] mb-[12px]">
+                            <form action="" method="post">
+                                <button type="submit" name="btn" value="<?php echo $plant['name'] ?>">
+                                    <div class="pt-[8px] cursor-pointer">
+                                        <div class="h-[82px] w-[82px] bg-[#5C7C5B] flex justify-center items-center border-2 rounded-lg border-[#496048]"><img class="w-[55px]" src="uploads/<?php echo $plant['cover_url'] ?>" alt="<?php echo $plant['name'] ?>"></div>
+                                    </div>
+                                </button>
+                            </form>
+                        </div>
+                    <?php endforeach ?>
+                </div>
             </section>
         </section>
+    </section>
     </div>
     <script>
         const addButton = document.getElementById("add");
