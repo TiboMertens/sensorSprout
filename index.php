7eb69fefb929853d874048655afccc06f6702af6
TiboMertens <tibomertens25@gmail.com>
2023-05-14 17:32:49 +0200

    chore: changed index page

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
 <?php
 include_once(__DIR__ . DIRECTORY_SEPARATOR . "bootstrap.php");
-try {
-    if (!empty($_POST['btn'])) {
-        //add laguage to session
-        if (!empty($_POST['language'])) {
-            $_SESSION['language'] = $_POST['language'];
-            header("Location: register.php");
-        } else {
-            throw new Exception("No language selected");
-        }
-    }
-} catch (Throwable $e) {
-    $error = $e->getMessage();
+
+if (!empty($_GET['qr_code'])) {
+    $qr_code = $_GET['qr_code'];
+    $sensor = Sensor::getSensorByQrCode($qr_code);
+
+    $sensor = $sensor['id'];
+    
 }
 
+if (!empty($_POST)) {
+    //get the username and password from the form
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //create a new user object
+    $user = new User();
+    if (!empty($sensor)) {
+        $user->setSensor($sensor);
+    }
+
+    //check if the user can login
+    try {
+        $result = $user->canLogin($username, $password);
+        //if the user can login, redirect to the index page
+        if ($result === true) {
+            $_SESSION['loggedin'] = true;
+            $_SESSION['id'] = $user->getId($username);
+
+            header("Location: home.php");
+            return;
+        }
+    } catch (throwable $e) {
+        $error = $e->getMessage();
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,7 +43,7 @@ try {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kies taal</title>
+    <title>Login</title>
     <link rel="stylesheet" href="css/styles.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -51,55 +72,40 @@ try {
         margin: 0;
         padding: 0;
     }
-
-    input[type="radio"] {
-        appearance: none;
-        -webkit-appearance: none;
-        border-radius: 50%;
-        border: 0px solid #FFF;
-        background: #F5F3F3;
-        /* The outline will be the outer circle */
-        outline: 1px solid #81CCDE;
-    }
-
-    input[type="radio"]:checked {
-        border: 3px solid #FFF;
-        background: #81CCDE;
-    }
 </style>
 
-<body class="bg-[#F5F3F3] ml-auto mr-auto max-w-[500px]" style="height: 100%;">
-    <section class="flex justify-center items-center" style="height:35%">
-        <p>LOGO</p>
-    </section>
-    <main class="bg-[#A5CF93] rounded-t-[30px] pl-[24px]" style="height: 65%">
-        <h1 class="text-[26px] text-white pt-[44px]">Selecteer uw taal</h1>
-        <div class="text-white text-[20px]">
-            <form action="#" method="post">
-                <!-- 3 radio buttons with the values 'nederlands' 'français' 'deutsch' -->
-                <div class="flex items-center mt-[14px]">
-                    <input type="radio" id="nederlands" name="language" value="nederlands" class="h-4 w-4">
-                    <label for="nederlands" class="ml-2">Nederlands</label>
-                </div>
-                <div class="flex items-center mt-[14px]">
-                    <input type="radio" name="language" value="français" id="français" class="h-4 w-4">
-                    <label for="français" class="ml-2">Français</label>
-                </div>
-                <div class="flex items-center mt-[14px]">
-                    <input type="radio" name="language" value="deutsch" id="deutsch" class="h-4 w-4">
-                    <label for="deutsch" class="ml-2">Deutsch</label>
-                </div>
-                <!-- If there is an error, show it -->
-                <?php if (isset($error)) : ?>
-                            <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
-                        <?php endif; ?>
-                <div class="mr-[24px] mt-[32px]">
-                    <input type="submit" value="GA VERDER" class="h-[48px] bg-[#81CCDE] w-full rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] tracking-[2px]" name="btn">
-                </div>
-            </form>
+<body class="bg-[#F5F3F3]" style="height: 100%;">
+    <div class="absolute inset-0 flex items-center justify-center ">
+        <div class="max-w-[500px]">
+            <section class="flex justify-center items-center" style="height:30%">
+                <p>LOGO</p>
+            </section>
+            <main class="bg-[#A5CF93] rounded-t-[5px] pl-[24px] pr-[24px] w-[400px]" style="height: 70%">
+                <form action="" method="post">
+                    <h1 class="text-[26px] text-white pt-[44px]">Log in</h1>
+                    <div class="text-white text-[20px]">
+                        <div class="mt-[16px]">
+                            <input type="text" placeholder="Gebruikersnaam" name="username" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px]">
+                        </div>
+                        <div class="mt-[32px]">
+                            <input type="password" placeholder="Wachtwoord" name="password" class="pl-[32px] font-bold w-full text-black h-[48px] rounded-[5px] text-[16px]">
+                            <a href="forgotPassword.php" class="block italic text-xs mt-1 underline text-black text-right hover:text-[#ffffff]">Forgot password?</a>
+                        </div>
+                    </div>
+                    <!-- If there is an error, show it -->
+                    <?php if (isset($error)) : ?>
+                        <p class="text-red-500 text-xs italic"><?php echo $error; ?></p>
+                    <?php endif; ?>
+                    <div class="mt-[32px]">
+                        <input type="submit" value="LOG IN" class="h-[48px] bg-[#81CCDE] w-full rounded-[5px] hover:bg-[#5EBCD4] font-bold text-[18px] text-white tracking-[2px]">
+                    </div>
+                    <div class="flex justify-center items-center text-xs mt-[32px] pb-10">
+                        <p class="text-center">Heeft u nog geen account? </br><a href="register.php"><span class="underline font-bold hover:text-white">Registreer u</span></a></p>
+                    </div>
+                </form>
+            </main>
         </div>
-    </main>
+    </div>
 </body>
-
 
 </html>
